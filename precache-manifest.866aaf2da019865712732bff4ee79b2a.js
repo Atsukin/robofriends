93a2da282dabd95ec3ccb6f8834f6ed484d5aeca
Atsukin <34465738+Atsukin@users.noreply.github.com>
2020-01-30 19:36:22 +0900

    Updates

---
+++ precache-manifest.866aaf2da019865712732bff4ee79b2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "db3237022a7cf74132da",
+    "url": "/robofriends/static/css/main.cb864c92.chunk.css"
+  },
+  {
+    "revision": "db3237022a7cf74132da",
+    "url": "/robofriends/static/js/main.db323702.chunk.js"
+  },
+  {
+    "revision": "cf3e224d2a2f8d014ce1",
+    "url": "/robofriends/static/css/1.302e3a26.chunk.css"
+  },
+  {
+    "revision": "cf3e224d2a2f8d014ce1",
+    "url": "/robofriends/static/js/1.cf3e224d.chunk.js"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "b423335b454e21410de24a2e423dd99e",
+    "url": "/robofriends/index.html"
+  }
+];
